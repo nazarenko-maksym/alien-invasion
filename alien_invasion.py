065f54971dd8345e7b00454c1cb876d2e5f00670
nazarenko-maksym <mrnazarenkom@gmail.com>
2026-03-31 21:41:26 -0400

    add condition for running the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -49,9 +49,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
             self.clock.tick(FPS)
 
