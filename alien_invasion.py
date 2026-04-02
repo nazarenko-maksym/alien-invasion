a76879bbb5bebc9f5649b5e67af688410ad1f344
nazarenko-maksym <mrnazarenkom@gmail.com>
2026-04-02 18:53:57 -0400

    add mouse cursor hiding while game is active

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
@@ -213,6 +216,8 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            # Make the mouse cursor visible
+            pygame.mouse.set_visible(True)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
