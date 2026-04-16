154be818394450c211d38299c02ce2929563ec72
nazarenko-maksym <mrnazarenkom@gmail.com>
2026-04-15 22:33:29 -0400

    refactor namings

--- bullet.py
+++ bullet.py
@@ -5,11 +5,11 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship."""
 
-    def __init__(self, ai_game):
+    def __init__(self, game):
         """Create a bullet object at the ship's current position."""
         super().__init__()
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
+        self.screen = game.screen
+        self.settings = game.settings
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position
@@ -17,7 +17,7 @@ class Bullet(Sprite):
             0, 0,
             self.settings.bullet_width, self.settings.bullet_height
         )
-        self.rect.midtop = ai_game.ship.rect.midtop
+        self.rect.midtop = game.ship.rect.midtop
 
         # Store the bullet's position as a float
         self.y = float(self.rect.y)
